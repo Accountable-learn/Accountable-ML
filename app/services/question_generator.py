c199ae9ea5b3bf8e2568f011808faefd127c3ab7
Tony Chan <tony3529353@gmail.com>
2024-06-26 21:34:55 -0400

    The output is a bit more stable and average generation time is around 8 seconds

--- app/services/question_generator.py
+++ app/services/question_generator.py
@@ -1,6 +1,7 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, BitsAndBytesConfig
 from langchain_core.prompts import PromptTemplate
+import time
 
 
 class QuestionGenerator:
@@ -12,7 +13,6 @@ class QuestionGenerator:
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_id)
         self.pad_token = self.tokenizer.eos_token  # Set pad_token to eos_token
 
-        # TODO: Output is not reliable
         self.bnb_config = BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_use_double_quant=True,
@@ -25,16 +25,13 @@ class QuestionGenerator:
             quantization_config=self.bnb_config
         )
 
-        self.prompt_template = PromptTemplate.from_template(
-            """
-You are an English teacher having a conversation with a student. Ask 10 questions about {topic}.
-Your format should be:
-Questions:
-1. ...
-2. ...
-            """
+        self.prompt_template = PromptTemplate(
+            input_variables=["topic"],
+            template=(
+                "You are an English teacher. Generate 10 open-ended questions for new English learners about the topic: {topic}.\n"
+                "Don't include the answers or annotation, only generate those 10 questions. \n"
+            )
         )
-
     def extract_questions(self, text: str) -> str:
         lines = text.split('\n')
         questions = [line.strip() for line in lines if line.strip().endswith('?')]
@@ -51,7 +48,12 @@ Questions:
         )
 
         # Generate the response
+        start_time = time.time()
         response = generator(prompt, max_new_tokens=1000)[0]['generated_text']
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Time taken to generate questions: {elapsed_time:.2f} seconds")
+
         questions = self.extract_questions(response)
         print(questions)
         return questions
